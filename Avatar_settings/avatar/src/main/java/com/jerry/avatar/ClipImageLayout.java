59588deecdfdbec05e355ee49d314649e8083ee1
Jerry <jerry@52xiaoluo.com>
2015-07-28 18:46:49 +0800

    1、初步实现了功能

---
+++ Avatar_settings/avatar/src/main/java/com/jerry/avatar/ClipImageLayout.java
@@ -0,0 +1,67 @@
+package com.jerry.avatar;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.widget.RelativeLayout;
+
+public class ClipImageLayout extends RelativeLayout {
+
+    private ClipZoomImageView mZoomImageView;
+    private ClipImageBorderView mClipImageView;
+
+    private int mHorizontalPadding = 60;
+    private int mVerticalPadding = 120;
+
+    public ClipImageLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mZoomImageView = new ClipZoomImageView(context);
+        mClipImageView = new ClipImageBorderView(context);
+
+        android.view.ViewGroup.LayoutParams lp = new LayoutParams(
+                android.view.ViewGroup.LayoutParams.MATCH_PARENT,
+                android.view.ViewGroup.LayoutParams.MATCH_PARENT);
+
+        this.addView(mZoomImageView, lp);//显示的图片
+        this.addView(mClipImageView, lp);//前面截取的边框
+
+
+        // 计算padding的px
+        mHorizontalPadding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, mHorizontalPadding, getResources().getDisplayMetrics());
+        mVerticalPadding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, mVerticalPadding, getResources().getDisplayMetrics());
+        mZoomImageView.setHorizontalPadding(mHorizontalPadding);
+       // mZoomImageView.setVerticalPadding(mVerticalPadding);
+        mClipImageView.setHorizontalPadding(mHorizontalPadding);
+        mClipImageView.setVerticalPadding(mVerticalPadding);
+    }
+
+    /**
+     * 对外公布设置边距的方法,单位为dp
+     *
+     * @param mHorizontalPadding
+     */
+    public void setHorizontalPadding(int mHorizontalPadding) {
+        this.mHorizontalPadding = mHorizontalPadding;
+    }
+
+    public void setmVerticalPadding(int mVerticalPadding) {
+        this.mVerticalPadding = mVerticalPadding;
+    }
+
+    /**
+     * 裁切图片
+     *
+     * @return
+     */
+    public Bitmap clip() {
+        return mZoomImageView.clip();
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        mZoomImageView.setImageBitmap(bitmap);
+    }
+
+}
---
+++ Avatar_settings/avatar/src/main/java/com/jerry/avatar/MainActivity.java
@@ -0,0 +1,249 @@
+package com.jerry.avatar;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    private RelativeLayout title_bar;
+    private TextView tv_title;
+    // private MyScrollView mScrollView;
+    private ImageView head;
+    private LinearLayout ll;
+
+    private PopupWindow popWindow;
+    private LayoutInflater layoutInflater;
+    private TextView photograph,albums;
+    private LinearLayout cancel;
+
+    public static final int PHOTOZOOM = 0; // 相册/拍照
+    public static final int PHOTOTAKE = 1; // 相册/拍照
+    public static final int IMAGE_COMPLETE = 2; // 结果
+    public static final int CROPREQCODE = 3; // 截取
+    private String photoSavePath;//保存路径
+    private String photoSaveName;//图pian名
+    private String path;//图片全路径
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        initView();
+    }
+
+    private void initView() {
+
+        layoutInflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        File file = new File(Environment.getExternalStorageDirectory(), "ClipHeadPhoto/cache");
+        if (!file.exists())
+            file.mkdirs();
+        photoSavePath=Environment.getExternalStorageDirectory()+"/ClipHeadPhoto/cache/";
+        photoSaveName =System.currentTimeMillis()+ ".png";
+
+        title_bar=(RelativeLayout) findViewById(R.id.title_bar);
+        tv_title=(TextView) findViewById(R.id.tv_title);
+
+        //  mScrollView=(MyScrollView) findViewById(R.id.mScrollView);
+        // mScrollView.setOnScrollListener(this);
+        head=(ImageView) findViewById(R.id.head);//头像
+        //  ll= (LinearLayout) findViewById(R.id.ll);//头像下部区域
+        //   ll.getBackground().setAlpha(200);
+
+        Bitmap bitmap = BitmapFactory.decodeFile(photoSavePath+"/head.png");
+        Drawable drawable = new BitmapDrawable(bitmap);
+       // head.setBackgroundDrawable(drawable);
+        head.setImageDrawable(drawable);
+
+        head.setOnClickListener(this);
+    }
+
+
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.head:
+                showPopupWindow(head);
+                break;
+        }
+    }
+
+    /**
+     * 设置显示的拍照,从相册选择这个PopupWindow的显示设置
+     * @param parent
+     */
+    @SuppressWarnings("deprecation")
+    private void showPopupWindow(View parent){
+        if (popWindow == null) {
+            View view = layoutInflater.inflate(R.layout.pop_select_photo,null);
+            popWindow = new PopupWindow(view,LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT,true);
+            initPop(view);
+        }
+        popWindow.setAnimationStyle(android.R.style.Animation_InputMethod);
+        popWindow.setFocusable(true);
+        popWindow.setOutsideTouchable(true);
+        popWindow.setBackgroundDrawable(new BitmapDrawable());
+        popWindow.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        popWindow.showAtLocation(parent, Gravity.CENTER, 0, 0);
+    }
+
+    /**
+     * 初始化PopupWindow,设置对应的监听事件
+     * @param view
+     */
+    public void initPop(View view){
+        photograph = (TextView) view.findViewById(R.id.photograph);//拍照
+        albums = (TextView) view.findViewById(R.id.albums);//相册
+        cancel= (LinearLayout) view.findViewById(R.id.cancel);//取消
+        photograph.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                popWindow.dismiss();
+                photoSaveName = String.valueOf(System.currentTimeMillis()) + ".png";
+                Uri imageUri = null;
+                Intent openCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                imageUri = Uri.fromFile(new File(photoSavePath, photoSaveName));
+                openCameraIntent.putExtra(MediaStore.Images.Media.ORIENTATION, 0);
+                openCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(openCameraIntent, PHOTOTAKE);
+            }
+        });
+        albums.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                popWindow.dismiss();
+                Intent openAlbumIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                openAlbumIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(openAlbumIntent, PHOTOZOOM);
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                popWindow.dismiss();
+
+            }
+        });
+    }
+
+    /**
+     * 图片选择及拍照结果
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        Uri uri = null;
+        switch (requestCode) {
+            case PHOTOZOOM://相册
+                if (data==null) {
+                    return;
+                }
+                uri = data.getData();
+                String[] proj = { MediaStore.Images.Media.DATA };
+                Cursor cursor = managedQuery(uri, proj, null, null,null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToFirst();
+                path = cursor.getString(column_index);// 图片在的路径
+                Intent intent3=new Intent(MainActivity.this, ClipActivity.class);
+                intent3.putExtra("path", path);
+                startActivityForResult(intent3, IMAGE_COMPLETE);
+                break;
+            case PHOTOTAKE://拍照
+                path=photoSavePath+photoSaveName;
+                uri = Uri.fromFile(new File(path));
+                Intent intent2=new Intent(MainActivity.this, ClipActivity.class);
+                intent2.putExtra("path", path);
+                startActivityForResult(intent2, IMAGE_COMPLETE);
+                break;
+            case IMAGE_COMPLETE:
+                final String temppath = data.getStringExtra("path");
+                head.setImageBitmap(getLoacalBitmap(temppath));
+                break;
+            default:
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    /**
+     * @param url
+     * @return
+     */
+    public static Bitmap getLoacalBitmap(String url) {
+        try {
+            FileInputStream fis = new FileInputStream(url);
+            return BitmapFactory.decodeStream(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public void onScroll(int scrollY) {
+        if(scrollY<30){
+            tv_title.setVisibility(View.GONE);
+            title_bar.setBackgroundColor(Color.parseColor("#00000000"));
+        }else{
+            tv_title.setVisibility(View.VISIBLE);
+            title_bar.setBackgroundColor(Color.parseColor("#18B4ED"));
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
